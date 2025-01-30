465f89a1c0a36ef6f20ff938bcb0a86d42e15022
Kenneth Okoronkwo <111024479+kenchigozie23@users.noreply.github.com>
2025-01-30 14:38:56 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const settings = {
   packname: 'Knight',
   author: 'Bot',
   botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botOwner: 'Andrax Botnet', // Your name
+  ownerNumber: '2347082039150', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
